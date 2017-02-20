75d7454c12933558b6f41b46435fb56137837e84
Nomesque <nomesk@gmail.com>
2017-02-20 21:55:44 +1000

    Added LevelManager object and script with LoadLevel function.

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string LevelName) {
+		Debug.Log("Level requested: " + LevelName);
+	}
+}
