1104322aa08214990869e13d73717c76f3aaeb42
Nomesque <nomesk@gmail.com>
2017-02-21 15:24:30 +1000

    Added link to next level in LoadLevel function.

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -5,6 +5,7 @@ public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel(string LevelName) {
 		Debug.Log("Level requested: " + LevelName);
+		Application.LoadLevel(LevelName);
 	}
 	
 	public void LeaveGame() {
