a36948c5d3a333c102ce9e89085d3df063c04e49
Nomesque <nomesk@gmail.com>
2017-02-21 15:07:06 +1000

    Added a LeaveGame function.

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -6,4 +6,9 @@ public class LevelManager : MonoBehaviour {
 	public void LoadLevel(string LevelName) {
 		Debug.Log("Level requested: " + LevelName);
 	}
+	
+	public void LeaveGame() {
+		Debug.Log("Leave request received.");
+	}
+	
 }
